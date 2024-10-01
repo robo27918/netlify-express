c91459d9559c6747e05b1b69ccc77830958265df
Roberto Toribio <roberttoribio04@gmail.com>
2024-09-30 19:06:36 -0700

    modified index.ejs

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -3,6 +3,6 @@ import serverless from 'serverless-http'
 const server = express()
 const router = Router()
 server.set('view engine', 'ejs')
-router.get('/', (req, res) => res.render('index'))
+router.get('/', (req, res) => res.render('index',{user:"Robert"}))
 server.use('/', router)
 export const handler = serverless(server)
