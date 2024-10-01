b5f28c52acc913767170a298bdab9be828c58eb2
Roberto Toribio <roberttoribio04@gmail.com>
2024-09-30 18:54:07 -0700

    fixed server.js file

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -4,6 +4,5 @@ const server = express()
 const router = Router()
 server.set('view engine', 'ejs')
 router.get('/', (req, res) => res.render('index'))
-ro
 server.use('/', router)
 export const handler = serverless(server)
