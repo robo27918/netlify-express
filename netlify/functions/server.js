3a469609c06d7e0d4557ea43e20049870c0b2091
Roberto Toribio <roberttoribio04@gmail.com>
2024-09-30 18:50:35 -0700

    Added functions

---
+++ netlify/functions/server.js
@@ -0,0 +1,9 @@
+import express, {Router} from 'express'
+import serverless from 'serverless-http'
+const server = express()
+const router = Router()
+server.set('view engine', 'ejs')
+router.get('/', (req, res) => res.render('index'))
+ro
+server.use('/', router)
+export const handler = serverless(server)
